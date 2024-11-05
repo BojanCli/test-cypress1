7411bd0e5945f65a0d06ee986d7b651952977fa8
unknown <bojan.krsteski@climedo.de>
2024-11-05 13:15:19 +0100

    Tests removed cos of secuity reasons

--- cypress/e2e/Homework2Bojan/Task1Visitsite.cy.js
+++
@@ -1,6 +0,0 @@
-describe('Authentication tests', () => {
-    it('should open login page', () => {
-        cy.visit('https://qa-1.climedo.de');       
-        cy.contains('Log in to start your session').should('be.visible');
-    });
-});
--- cypress/e2e/Homework2Bojan/Task5Class.js
+++
@@ -1,9 +0,0 @@
-class Login {    
-    login(username, password) {    
-        cy.get('input[placeholder="E-mail"]').type(username);
-        cy.get('input[placeholder="Password"]').type(password);
-        cy.get('button[type="submit"]').click();
-        cy.contains('Auto logout in').should('be.visible');
-    };
-};
-export default Login
